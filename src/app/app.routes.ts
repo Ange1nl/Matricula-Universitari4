60650617b648639ed14a4319c835e9e8fed6cbc7
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-06-21 18:28:30 -0500

    Creacion de login de estudiante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { NotFoundPage } from './pages/not-found-page/not-found-page';
+import { InicioEstudiante } from './features/estudiante/inicio-estudiante/inicio-estudiante';
 
 export const routes: Routes = [
 
     {path: 'inicio', component:Inicio, title: "Pagina principal"},
+    {path: '',redirectTo:'inicio', pathMatch:'full'},//si esque esta vacio es decir localhost:4200 manda a inicio
+    {path: 'inicioEstudiante',component:InicioEstudiante,title:"Login estudiante"},
+
 
     {path: '**', component:NotFoundPage, title: "Pagina no encontrada"}
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/header/header";
 import { Footer } from "./shared/components/footer/footer";
+import { InicioEstudiante } from "./features/estudiante/inicio-estudiante/inicio-estudiante";
 
 @Component({
   selector: 'app-root',
