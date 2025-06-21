e779c284c0501193cf0f6a886d931daaee591b6d
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-06-21 12:50:40 -0500

    Creacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { NotFoundPage } from './pages/not-found-page/not-found-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'inicio', component:Inicio, title: "Pagina principal"},
+
+    {path: '**', component:NotFoundPage, title: "Pagina no encontrada"}
+
+
+
+];
