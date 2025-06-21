ee35a88b46e18813dc8d2bdf3564a88c8fd9ead5
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-06-20 23:08:10 -0500

    Creacion del recep

---
+++ src/app/features/recep/panel-recep/panel-recep.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-recep',
+  imports: [],
+  templateUrl: './panel-recep.html',
+  styleUrl: './panel-recep.css'
+})
+export class PanelRecep {
+
+}
