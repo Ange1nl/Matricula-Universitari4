c4f7e4014ac4cc1c156311549cd24139cc81c550
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-06-21 20:30:05 -0500

    renombrar y correcion

---
+++ src/app/features/estudiante/sesion-estudiante/sesion-estudiante.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sesion-estudiante',
+  imports: [],
+  templateUrl: './sesion-estudiante.html',
+  styleUrl: './sesion-estudiante.css'
+})
+export class SesionEstudiante {
+
+}
