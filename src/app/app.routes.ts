a9632aeb07bc1c76faf2d48280722e593efa44d0
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-06-24 19:37:27 -0500

    Html para matriculas realizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SesionEstudiante } from './features/estudiante/sesion-estudiante/sesion
 import { RegistroEstudiante } from './features/estudiante/registro-estudiante/registro-estudiante';
 import { PublicLayout } from './layouts/public-layout/public-layout';
 import { PrivateLayout } from './layouts/private-layout/private-layout';
+import { MatriculaEstudiante } from './features/estudiante/matricula-estudiante/matricula-estudiante';
 
 export const routes: Routes = [
 
@@ -18,22 +19,22 @@ export const routes: Routes = [
     */
 
 
-    {path: '',component: PublicLayout,title:'Pagina publica',
+    {path: '',component: PublicLayout,
         children: [
-        { path: 'inicio', component: Inicio, title: "Página principal" },
-        { path: '', redirectTo: 'inicio', pathMatch: 'full' },//si esque esta vacio es decir localhost:4200 manda a inicio
-        { path: 'sesionEstudiante', component: SesionEstudiante, title: "Login estudiante" },
-        { path: 'registroEstudiante', component: RegistroEstudiante, title: "Registro estudiante" }
+            { path: '', redirectTo: 'inicio', pathMatch: 'full' },//si esque esta vacio es decir localhost:4200 manda a inicio
+            { path: 'inicio', component: Inicio, title: "Página principal" },
+            { path: 'sesionEstudiante', component: SesionEstudiante, title: "Login estudiante" },
+            { path: 'registroEstudiante', component: RegistroEstudiante, title: "Registro estudiante" },
         ]
     },
 
 
-    {path: '',component: PrivateLayout,title:'Paginas privada',
-    children: [
-      //{ path: 'matricula', component: MatriculaComponent, title: "Matrícula" },
-      //{ path: 'panel-admin', component: PanelAdmin, title: "Panel administrador" }
-    ]
-  },
+    {path: '',component: PrivateLayout,
+        children: [
+            { path: 'matricula', component: MatriculaEstudiante, title: "Matrícula" },
+            //{ path: 'panel-admin', component: PanelAdmin, title: "Panel administrador" }
+        ]
+    },
 
   { path: '**', component: NotFoundPage, title: "Página no encontrada" }
 
