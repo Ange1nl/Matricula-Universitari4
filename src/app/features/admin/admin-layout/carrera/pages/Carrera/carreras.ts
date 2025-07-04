20afdd9bdf904e251c3725d73167245a5dbf0241
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-07-04 01:42:56 -0500

    Validadores de backend en carrera

--- src/app/features/admin/admin-layout/carrera/pages/Carrera/carreras.ts
+++ src/app/features/admin/admin-layout/carrera/pages/Carrera/carreras.ts
@@ -1,28 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { CarreraService } from '../../services/carrera.service';
 import { Carrera } from '../../models/carrera';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-carreras',
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './carreras.html',
   styleUrl: './carreras.css'
 })
 export class Carreras {
 
+  protected carrera$!: Observable<Carrera[]>;
   private serv = inject(CarreraService);
   private fb = inject(FormBuilder);
 
-  carreras: Carrera[] = [];
+  //carreras: Carrera[] = [];
   mostrarModal: boolean = false;
   carreraSeleccionada: Carrera = { id_carrera: 0, nombre_carrera: '' }; // Objeto que almacena temporalmente la carrera a editar
 
   //Formulario reactivo para agregar una carrera
   agregarForm: FormGroup = this.fb.group({
-    nombre_carrera: ['', Validators.required]
+    nombre_carrera: ['',Validators.required]
   });
 
   // Formulario reactivo para editar una carrera en modal
@@ -39,9 +41,12 @@ export class Carreras {
 
 
   obtenerCarreras(): void {
+    this.carrera$ = this.serv.listar(); //Asignacion directa al observable
+    /*
     this.serv.listar().subscribe(data => {
-      this.carreras = data; //data es la respuesta del servidor  y lo guardamos en carreras que sera despues mostrado en el html
+      this.carreras = data; //data es la respuesta del servidor  y lo guardamos en carreras que sera despues mostrado en el html , Suscripcion manual al observable
     });
+    */
   }
 
   agregarCarrera(): void {
@@ -49,9 +54,29 @@ export class Carreras {
     //Se crea un objeto carrera con los datos ingresados del formulario reactivo
     const nueva: Carrera = this.agregarForm.value;
 
-    this.serv.insertar(nueva).subscribe(() => {
-      this.agregarForm.reset();
-      this.obtenerCarreras(); // Recarga la lista de carreras actualizada
+    this.serv.insertar(nueva).subscribe({
+
+      next: () => {
+        this.agregarForm.reset();
+        this.obtenerCarreras(); // Recarga la lista de carreras actualizada
+      },
+
+      error: (err) => {
+      // Verifica si el error es de validación del backend (código HTTP 400 - Bad Request)
+      if (err.status === 400 && err.error) { //El err.error que recibes en Angular es exactamente el mismo objeto que construyes en el backend
+        const errores = err.error; // Map<String, String> del backend ,  // { nombre_carrera: "No puede contener números" }
+        for (const campo in errores) { //Recorre cada clave del JSON que envió el backend.
+          // Busca el control (input) correspondiente en el formulario
+          const control = this.agregarForm.get(campo);// ejemplo: agregarForm.get('nombre_carrera') , Intenta buscar un campo con ese nombre en el formulario reactivo
+          if (control) { //Se asegura de que sí existe ese campo en el formulario 
+            control.setErrors({ backend: errores[campo] }); 
+            //"backend" es la clave personalizada del error ,backend , es no puede contener numeros por ejemplo
+            //errores[campo] es el mensaje de error que envió el backend , campo es nombre_carrera
+          }
+        }
+      }
+      }
+      
     });
   }
 
@@ -73,7 +98,7 @@ export class Carreras {
 
     // Se cargan los valores actuales de la carrera en el formulario de edición
     this.editarForm.setValue({
-      id_carrera: carrera.id_carrera,
+      id_carrera: carrera.id_carrera, //El primero id_carrera es lo del formulario, el segundo es lo que capturo y ese segundo que captura sera colocado en el formulario del modal de editar
       nombre_carrera: carrera.nombre_carrera
     });
   }
