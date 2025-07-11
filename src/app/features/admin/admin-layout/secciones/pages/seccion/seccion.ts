920d9b847c98a6c547a5a0420a7b66406d8b0a39
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-07-10 19:27:03 -0500

    CRUD con relaciones fk de seccion listo

--- src/app/features/admin/admin-layout/secciones/pages/seccion/seccion.ts
+++ src/app/features/admin/admin-layout/secciones/pages/seccion/seccion.ts
@@ -3,26 +3,36 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DocenteService } from '../../../docente/services/docente.service';
 import { Docente } from '../../../docente/models/docente';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { CursoInfocursoService } from '../../../carrera/services/curso-infocurso.service';
 import { Curso } from '../../../carrera/models/curso';
 import { SeccionService } from '../../services/seccion.service';
+import { SeccionesModels } from '../../models/secciones-models';
+import { SeccionEnvioModels } from '../../models/seccion-envio-models';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-seccion',
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, AsyncPipe],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, AsyncPipe,RouterLink],
   templateUrl: './seccion.html',
   styleUrl: './seccion.css'
 })
 export class Seccion {
 
-  private serv = inject(DocenteService);
-  private servi = inject(CursoInfocursoService);
+  private docenServ = inject(DocenteService);
+  private cursoServ = inject(CursoInfocursoService);
   private seccionServ = inject(SeccionService);
   private fb = inject(FormBuilder);
 
   protected docen$!: Observable<Docente[]>;
   protected curso$!: Observable<Curso[]>;
+  protected secciones$!: Observable<SeccionesModels[]>;
+
+  /*
+  private seccionesSubject = new BehaviorSubject<SeccionesModels[]>([]);
+  protected secciones$ = this.seccionesSubject.asObservable();
+  */
+
 
 
   formulario: FormGroup = this.fb.group({
@@ -35,21 +45,52 @@ export class Seccion {
   });
 
 
-
   ngOnInit() {
     this.cargarDocentes();
-    this.obtenerCursos();
+    this.cargarCursos();
+    this.obtenerSecciones();
   }
 
-
+  //PARA MI COMBO BOX SE MUESTRE LOS DOCENTES NOMAS ES
   cargarDocentes() {
-    this.docen$ = this.serv.listar();
+    this.docen$ = this.docenServ.listar();
   }
 
-  obtenerCursos(): void {
-    this.curso$ = this.servi.listarCurso();
+  //PARA MI COMBO BOX SE MUESTRE LOS CURSOS NOMAS ES
+  cargarCursos(): void {
+    this.curso$ = this.cursoServ.listarCurso();
   }
 
+
+  obtenerSecciones() {
+    this.secciones$ = this.seccionServ.listar();
+    /*this.seccionServ.listar().subscribe({
+      next: (data) => this.seccionesSubject.next(data) //data es lo que me devuelve del service es decir hago la peticion get, el backend responde y es este data que se agregara al array
+    });*/
+  }
+
+  cancelarEdicion() {
+    this.editando = false;
+    this.idEditando = 0;
+    this.formulario.reset();
+  }
+
+  editando: boolean = false;
+  idEditando!: number;
+
+  editarSeccion(seccion: SeccionesModels) {
+    this.editando = true;
+    this.idEditando = seccion.idSeccion; //Captura el idSeccion
+
+    this.formulario.patchValue({ //Como ya capturo el idSeccion es colocado en los inputs del html sus demas datos
+      id_curso: seccion.curso.id_curso,
+      horario: seccion.horario,
+      aula: seccion.aula,
+      id_profesor: seccion.profesores.id_profesor,
+      cupos: seccion.cupos,
+      modalidad: seccion.modalidad
+    });
+  }
 
 
   agregarSeccion() {
@@ -58,25 +99,45 @@ export class Seccion {
 
     const form = this.formulario.value;
 
-    const nuevaSeccion = {
-      curso: { id_curso: form.id_curso },
+    const seccionEnvio: SeccionEnvioModels = { //Pongo la interfaz por buena practica , solo asegura el tipado y que coincida tanto aca cuando envio al backend
+      curso: { id_curso: form.id_curso }, //Para que en curso se mande solo el id_curso es decir curso = id_curso
       horario: form.horario,
       aula: form.aula,
-      profesores: { id_profesor: form.id_profesor },
+      profesores: { id_profesor: form.id_profesor }, //profesores = id_profesor
       cupos: form.cupos,
       modalidad: form.modalidad
     };
-    /*
-    this.seccionServ.insertar(nuevaSeccion).subscribe({
-      next: (data) => {
-        console.log('Sección insertada con éxito:', data);
-        this.formulario.reset();
-      },
-      error: (err) => {
-        console.error('Error al insertar sección:', err);
-      }
-    });*/
 
+    if (this.editando) {
+      this.seccionServ.editar(this.idEditando, seccionEnvio).subscribe({
+        next: () => {
+          this.editando = false;
+          this.idEditando = 0;
+          this.formulario.reset();
+          this.obtenerSecciones();
+        },
+        error: (err) => console.error('Error al editar:', err)
+      });
+    } else {
+      this.seccionServ.insertar(seccionEnvio).subscribe({
+        next: () => { //El data es lo que me devuelve, asi next (data), cuando hago el post , me devuelve en JSON y es ese data ese JSON devuelto, aca no tiene data , pero igual dejo el comentario xd        
+          this.formulario.reset();
+          this.obtenerSecciones(); // actualiza tabla
+        },
+        error: (err) => console.error('Error al insertar:', err)
+      });
+    }
+  }
+
+
+  eliminarSeccion(id: number) {
+    if (!confirm('¿Está seguro de eliminar esta sección?')) return;
+    this.seccionServ.eliminar(id).subscribe({
+      next: () => {},
+      error: (err) => {
+        console.error('Error al eliminar sección:', err);
+      }
+    });
   }
 
 
--- src/app/features/admin/admin-layout/secciones/services/seccion.service.ts
+++ src/app/features/admin/admin-layout/secciones/services/seccion.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SeccionesModels } from '../models/secciones-models';
+import { SeccionEnvioModels } from '../models/seccion-envio-models';
 
 @Injectable({
   providedIn: 'root'
@@ -15,16 +16,18 @@ export class SeccionService {
     return this.http.get<SeccionesModels[]>(`${this.URL}/listar`);
   }
 
-  insertar(seccionModels: SeccionesModels): Observable<SeccionesModels> {
-    return this.http.post<SeccionesModels>(`${this.URL}/insertar`, seccionModels);
+  //Consume este metodo el servidor me responde en formato JSON con el tipado SeleccionesModels pero no lo uso porque generalmente eso se usa con el BehaviorSubject es decir me devuelve el dato y en el componente , cuando me suscribo, next(data) , en data esta lo que me devuelve y eso puedo ponerlo al behavior subject y actualizar simplemente el array
+  //pero como aca tiene relacion con otras entidades , cuando es POST y PUT solo me devuelve los ids y los demas campos de los FK son nulos y eso hace en el html que algunos campos de los fk no se muestren . En cambio en get internamente busca la relacion y si te devuelve todo los datos, comprobado con postman
+  insertar(seccionEnvioModels: SeccionEnvioModels): Observable<SeccionesModels> {
+    return this.http.post<SeccionesModels>(`${this.URL}/insertar`, seccionEnvioModels);
   }
 
-  editar(id: number, seccionModels: SeccionesModels): Observable<SeccionesModels> {
-    return this.http.put<SeccionesModels>(`${this.URL}/actualizar/${id}`, seccionModels);
+  editar(id: number, seccionEnvioModels: SeccionEnvioModels): Observable<SeccionesModels> {
+    return this.http.put<SeccionesModels>(`${this.URL}/actualizar/${id}`, seccionEnvioModels);
   }
 
-  eliminar(id: number): Observable<SeccionesModels> {
-    return this.http.delete<SeccionesModels>(`${this.URL}/eliminar/${id}`);
+  eliminar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.URL}/eliminar/${id}`);
   }
 
 }
