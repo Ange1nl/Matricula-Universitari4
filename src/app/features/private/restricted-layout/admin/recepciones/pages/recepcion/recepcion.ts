d875b251b2ac5a944427fa98bdf0a7e586881b4a
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-07-12 01:20:25 -0500

    Creacion de componente y servicio recepcion

---
+++ src/app/features/private/restricted-layout/admin/recepciones/pages/recepcion/recepcion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepcion',
+  imports: [],
+  templateUrl: './recepcion.html',
+  styleUrl: './recepcion.css'
+})
+export class Recepcion {
+
+}
